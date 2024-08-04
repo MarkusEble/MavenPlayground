3b63cdc5699f4fcf55fe746cdd9549759560de56
Markus Eble <markus.eble@online.de>
2024-08-04 17:47:21 +0200

    StateMachine skeleton with test

---
+++ src/test/java/com/ABMachineTest.java
@@ -0,0 +1,24 @@
+package com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ABMachineTest {
+
+    @Test
+    public void testAccept() throws Exception {
+        com.compiler.StateMachine abMachine = new com.compiler.machines.ABMachine();
+        java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
+        abMachine.process("AABBBB", outWriter);
+        assertTrue(abMachine.isFinalState());
+    }
+
+    @Test
+    public void testNoAccept() throws Exception {
+        com.compiler.StateMachine abMachine = new com.compiler.machines.ABMachine();
+        java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
+        abMachine.process("BBBBAA", outWriter);
+        assertTrue(!abMachine.isFinalState());
+    }
+}
