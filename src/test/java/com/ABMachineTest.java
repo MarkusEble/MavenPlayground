c97b182239658ceb9cb403c6bde448b9caa44438
Eble <markus.eble@sap.com>
2025-01-02 14:27:34 +0100

    generic machine test

--- src/test/java/com/ABMachineTest.java
+++ src/test/java/com/ABMachineTest.java
@@ -1,32 +1,20 @@
 package com;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-public class ABMachineTest {
+public class ABMachineTest extends MachineTestBase {
+
+    private void testWord(String word, Boolean expectOk) throws Exception {
+        testWord(new com.compiler.machines.ABMachine(), word, expectOk);
+    }
 
     @Test
     public void testAccept() throws Exception {
-        com.compiler.StateMachine abMachine = new com.compiler.machines.ABMachine();
-        java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-        abMachine.process("AABBBB", outWriter);
-        assertTrue(abMachine.isFinalState());
+        testWord("AABBBB", true);
     }
 
     @Test
     public void testNoAccept() throws Exception {
-        com.compiler.StateMachine abMachine = new com.compiler.machines.ABMachine();
-        java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-        abMachine.process("BBBBAA", outWriter);
-        assertTrue(!abMachine.isFinalState());
-    }
-
-    @Test
-    public void testNoAccept2() throws Exception {
-        com.compiler.StateMachine abMachine = new com.compiler.machines.ABMachine();
-        java.io.OutputStreamWriter outWriter = new java.io.OutputStreamWriter(System.out);
-        abMachine.process("A", outWriter);
-        assertTrue(!abMachine.isFinalState());
+        testWord("BBBBAA", false);
     }
 }
