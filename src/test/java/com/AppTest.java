7428ac5456054d4a7c7dfb83a47f2e7ae01450a9
Eble <markus.eble@sap.com>
2025-03-23 12:46:42 +0100

    open resource file

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -2,6 +2,12 @@ package com;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.Test;
 
 /**
@@ -13,8 +19,19 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws Exception
     {
         assertTrue( true );
+        String testSuiteContent = com.compiler.InputReader.fileToString("src/test/resources/test.txt");
+    }
+
+    private String readFile(String fileName) throws Exception {
+        java.net.URL url = getClass().getClassLoader().getResource(fileName);
+        if (url == null) {
+            throw new IllegalArgumentException("File not found! " + fileName);
+        } else {
+            java.nio.file.Path resPath = Paths.get(url.toURI());
+            return new String(Files.readAllBytes(resPath), "UTF8");
+        }
     }
 }
